3fd14bf34eba9bdd530f135831c9fa8039e34500
zhaohuan <1987287033@qq.com>
2017-12-02 23:37:15 +0800

    add 5

---
+++ 05/login/src/components/Music.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <mt-badge type="warning" size="large">音乐世界欢迎你</mt-badge>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
